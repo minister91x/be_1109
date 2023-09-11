72e7e420465795cec456ca477956e444d8f79924
quan91 <quan91.nt@gmail.com>
2023-09-11 21:41:47 +0700

    buoi1_Routing

---
+++ WebMvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMvcDemo.Models;
+
+namespace WebMvcDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            return RedirectToAction("About", "Home");
+            var listModel = new StudentModels_TrungGian();
+            var list = new List<StudentModels>();
+            var listNew = new List<StudentModels_New>();
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var model = new StudentModels
+                    {
+                        Id = i,
+                        Name = "NGUYEN VAN" + i
+                    };
+
+                    list.Add(model);
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var model2 = new StudentModels_New
+                    {
+                        Id = i,
+                        Name = "NGUYEN VAN NEW" + i
+                    };
+
+                    listNew.Add(model2);
+                }
+
+
+                listModel.studentModels = list;
+                //listModel.StudentModels_New = listNew;
+
+                ViewBag.studentModels = listNew;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            return View(listModel);
+        }
+
+        public ActionResult About(string name)
+        {
+            ViewBag.Message = "Your Name:" + name;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebMvcDemo/Models/StudentModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMvcDemo.Models
+{
+    public class StudentModels
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class StudentModels_New
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class StudentModels_TrungGian
+    {
+        public List<StudentModels> studentModels { get; set; }
+        public List<StudentModels_New> StudentModels_New { get; set; }
+        public StudentModels studentModel { get; set; }
+    }
+}
