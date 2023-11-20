d8e3016b0710b354108f778d9f3b41eb370f0e32
quan91 <quan91.nt@gmail.com>
2023-11-20 21:54:27 +0700

    buoi 13

---
+++ BE1109/Controllers/ShoppingCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BE1109.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
